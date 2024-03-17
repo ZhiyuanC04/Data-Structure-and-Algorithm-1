d1f46b40ee1ab6e2a634a41a73c5feba740927c3
ZhiyuanC04 <158787196+ZhiyuanC04@users.noreply.github.com>
2024-03-17 17:59:34 -0400

    Delete Labs directory

--- Labs/Lab 03/src/Horse.java
+++
@@ -1,9 +0,0 @@
-public class Horse extends Animal{
-
-    public Horse(String nm)     {
-        super(nm);    // builds ala parent
-    }
-    public void speak()  {  // this method specific to Cow
-        System.out.println("??");
-    }
-}
--- Labs/Lab 03/src/Working.java
+++
@@ -1,4 +0,0 @@
-public interface Working {
-    public void work();
-}
-
